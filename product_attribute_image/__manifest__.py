dbea7d86e228ced2c3a4f822c46ab3d86eee8154
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-09-04 12:05:21 +0200

    [13.0][ADD] product_attribute_image

---
+++ product_attribute_image/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Attribute Image",
+    "summary": """
+        This module adds the ability to add an image to an attribute value""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow S.L.,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
+    "depends": ["sale"],
+    "data": ["views/product_attribute_views.xml"],
+}
