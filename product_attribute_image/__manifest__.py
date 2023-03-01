031f5294f41fb1b4173c898cfdbfe324f5e71a1a
Miquel Raïch <miquel.raich@forgeflow.com>
2023-03-01 13:37:58 +0100

    [MIG] product_attribute_image: Migration to 15.0

--- product_attribute_image/__manifest__.py
+++ product_attribute_image/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Attribute Image",
     "summary": """
         This module adds the ability to add an image to an attribute value""",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow S.L.,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-variant",
